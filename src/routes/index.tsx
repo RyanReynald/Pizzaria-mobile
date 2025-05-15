feeb26bb49f11be3b3551288f3351d49165d6c71
Ryan Reynald <ryanreynald67@gmail.com>
2025-05-15 15:57:58 -0300

    Finalização do projeto mobile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,35 +1,36 @@
 import React, { useContext } from 'react';
 
-import { View, ActivityIndicator } from 'react-native';
+import {View, ActivityIndicator} from 'react-native';
 
 import AppRoutes from './app.routes';
 import AuthRoutes from './auth.routes';
 
-import { AuthContext } from '../contexts/AuthContext';
+import { AuthContext } from '../contexts/AuthContext'
 
-function Routes() {
-    const { isAuthenticated, loading } = useContext(AuthContext);
-    
+function Routes(){
+  const { isAuthenticated, loading } = useContext(AuthContext);
 
-    if (loading) {
-        return (
-            <View style={{
-                flex: 1,
-                backgroundColor: '#1d1d2e',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-        >
-            <ActivityIndicator size={60} color="#FFF"/>
-            </View>
-        )
-    }
 
-    return (
-        isAuthenticated ? <AppRoutes /> : <AuthRoutes />
+  if(loading){
+    return(
+      <View 
+        style={{ 
+          flex:1, 
+          backgroundColor: '#1D1D2E', 
+          justifyContent: 'center', 
+          alignItems:'center' 
+        }}
+      >
+        <ActivityIndicator size={60} color="#FFF" />
+      </View>
     )
-}
+  }
 
+
+  return(
+    isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
+  )
+}
 
 
 export default Routes;
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3333'
-  baseURL: 'http://192.168.1.10:3333'
+  baseURL: 'http://172.21.36.111:3333'
 })
 
 export { api };
